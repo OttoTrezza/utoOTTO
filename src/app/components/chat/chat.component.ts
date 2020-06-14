33f43e69168033a3c850b9b5c6dcec50d33e8ec6
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2020-06-14 19:21:47 -0300

    con servicio

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -69,7 +69,7 @@ export class ChatComponent implements OnInit, OnDestroy {
       this.gamma1 = Math.round(event.gamma);
       this.alpha1 = Math.round(event.alpha);
 
-     // this.sendElSarmiento(this.beta1, this.gamma1, this.alpha1);
+     this.sendElSarmiento(this.beta1, this.gamma1, this.alpha1);
     });
 
 
